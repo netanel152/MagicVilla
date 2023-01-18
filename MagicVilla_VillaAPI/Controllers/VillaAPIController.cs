f0eaf6bbe13ad667c12330b5dc4bb37768f18a7d
netanel152 <Netanelos152@gmail.com>
2023-01-18 15:34:01 +0200

    First Controller and add some data

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            var villaList = VillaStore.villaList;
+            if (villaList.Count == 0)
+            {
+                return NotFound("No villas in the list");
+            }
+            return Ok(villaList);
+        }
+
+        [HttpGet("id:int")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();  
+            }
+            return Ok(villa);
+        }
+    }
+}
